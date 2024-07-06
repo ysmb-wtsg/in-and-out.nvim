ab24cafadc3418dffb0c7e9b0621cff60b9ac551
Peter Aronoff <peter@aronoff.org>
2024-07-05 23:52:08 -0400

    feat: add (optional) setup for configuration (#5)

--- lua/in-and-out/init.lua
+++ lua/in-and-out/init.lua
@@ -15,15 +15,31 @@ local function escape_lua_pattern(s)
 		["-"] = "%-",
 		["?"] = "%?",
 	}
-	return (s:gsub(".", matches))
+	return s:gsub(".", matches)
 end
 
 local targets = { '"', "'", "(", ")", "{", "}", "[", "]", "`" }
 
+function M.setup(config)
+	vim.validate("config", config, "table", true)
+	vim.validate("config.targets", config.targets, "table", true)
+	vim.validate("config.additional_targets", config.additional_targets, "table", true)
+
+	if config and config.targets then
+		targets = config.targets
+		return
+	end
+
+	if config and config.additional_targets then
+		for _, target in ipairs(config.additional_targets) do
+			targets[#targets + 1] = target
+		end
+	end
+end
+
 function M.in_and_out()
 	local current_row, current_col = unpack(vim.api.nvim_win_get_cursor(0))
-	local line_count_in_buf = vim.api.nvim_buf_line_count(0)
-	local lines_in_buf = vim.api.nvim_buf_get_lines(0, current_row - 1, line_count_in_buf, false)
+	local lines_in_buf = vim.api.nvim_buf_get_lines(0, current_row - 1, -1, false)
 
 	local target_row = 0
 	local target_col = nil
@@ -31,7 +47,8 @@ function M.in_and_out()
 		for _, char in ipairs(targets) do
 			local found_col = string.find(line, escape_lua_pattern(char), current_col + 1)
 			if found_col and (not target_col or found_col < target_col) then
-				target_col = found_col
+				-- If char is a multibyte character, we need to take into account the extra bytes.
+				target_col = found_col + vim.fn.strlen(char) - 1
 			end
 		end
 		if target_col then
@@ -42,8 +59,6 @@ function M.in_and_out()
 	end
 	if target_col then
 		vim.api.nvim_win_set_cursor(0, { target_row, target_col })
-	else
-		print("next rarget not found.")
 	end
 end
 
